0abc153c510f51836407cc8b337918fbe46c60b4
Frank Ittermann <frank.ittermann@yahoo.de>
2020-07-12 20:15:10 +0200

    Enable unlimited swap memory for docker jocker agents

--- shared-library/vars/jenkins/DockerAgentBootstrap.groovy
+++ shared-library/vars/jenkins/DockerAgentBootstrap.groovy
@@ -12,6 +12,7 @@ node('docker') {
                 sh("docker version")
                 sh(
                     """
+                    docker pull ${params.image}
                     docker run -d --name ${params.node} --memory-swap=-1 --rm ${params.image} -noreconnect -url ${env.JENKINS_MASTER} ${secret} ${params.node}
                     """
                 )
